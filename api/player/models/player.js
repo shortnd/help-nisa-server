3b5727194cd81f8ce26d16497a34ba5483af9a1e
Collin OConnell <ococncol@gmail.com>
2020-02-28 15:08:43 -0500

    trying to add slug

--- api/player/models/player.js
+++ api/player/models/player.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const slugify = require('slugify');
+
 /**
  * Lifecycle callbacks for the `player` model.
  */
@@ -7,7 +9,11 @@
 module.exports = {
   // Before saving a value.
   // Fired before an `insert` or `update` query.
-  // beforeSave: async (model, attrs, options) => {},
+  beforeSave: async (model) => {
+    if (model.name) {
+      model.slug = slugify(model.name)
+    }
+  },
 
   // After saving a value.
   // Fired after an `insert` or `update` query.
@@ -39,7 +45,13 @@ module.exports = {
 
   // Before updating a value.
   // Fired before an `update` query.
-  // beforeUpdate: async (model, attrs, options) => {},
+  beforeUpdate: async model => {
+    if (model.getUpdate() && model.getUpdate().title) {
+      model.update({
+        slug: slugify(model.getUpdate().title)
+      })
+    }
+  },
 
   // After updating a value.
   // Fired after an `update` query.
